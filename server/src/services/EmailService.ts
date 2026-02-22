9edd4b250ded613d26ae2ba1549fa45b4a51faff
shafarizkyf <shafarizkyf@gmail.com>
2026-02-22 17:20:18 +0700

    add email notification

--- server/src/services/EmailService.ts
+++ server/src/services/EmailService.ts
@@ -1,5 +1,7 @@
 import { Resend } from 'resend';
 import { config } from '../config';
+import * as fs from 'fs';
+import * as path from 'path';
 
 type Send = {
   to: string;
@@ -7,13 +9,54 @@ type Send = {
   html: string;
 }
 
+type SendTrackingUpdate = {
+  to: string;
+  awb: string;
+  courier: string;
+  latestStatus: string;
+  history: Array<{
+    date: string;
+    location: string;
+    description: string;
+  }>;
+}
+
 export class EmailService {
+  private static getTemplate(templateName: string): string {
+    const templatePath = path.join(__dirname, '..', 'templates', templateName);
+    return fs.readFileSync(templatePath, 'utf-8');
+  }
+
   static async send({ to, subject, html}: Send) {
     const resend = new Resend(config.resend.apiKey);
     resend.emails.send({
       from: config.resend.from,
       to,
       subject,
+      html
+    });
+  }
+
+  static async sendTrackingUpdate({ to, awb, courier, latestStatus, history }: SendTrackingUpdate) {
+    const template = this.getTemplate('status-update.html');
+
+    const historyRows = history.map(h => `
+      <tr>
+        <td>${h.date}</td>
+        <td>${h.location}</td>
+        <td>${h.description}</td>
+      </tr>
+    `).join('');
+
+    const html = template
+      .replace('{{AWB}}', awb)
+      .replace('{{COURIER}}', courier)
+      .replace('{{LATEST_STATUS}}', latestStatus)
+      .replace('{{HISTORY_ROWS}}', historyRows);
+
+    await this.send({
+      to,
+      subject: `[${awb}] ${latestStatus}`,
       html
     });
   }
